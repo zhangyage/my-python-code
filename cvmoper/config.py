dc045b414b2c4113d01483af22d077ec09b491f4
redhatxl <18329903316@163.com>
2018-08-27 16:55:49 +0800

    change fiel

--- cvmoper/config.py
+++ cvmoper/config.py
@@ -7,7 +7,7 @@
 SecretId = AKIDjPYbTBU4FF4xxxxxxxxxxxxxxxxxxxxxx
 # 腾讯云secretkey
 SecretKey = e7RaXYVP63rUvBNUQF1iKxxxxxxxx
-# cvm 所在地域
+# cvm 所在地域,可参考https://cloud.tencent.com/document/api/213/15708
 Region = ap-shanghai
 
 # 腾讯云cvm实例id，多个用,隔开
